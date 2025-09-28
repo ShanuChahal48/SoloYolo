338ffad4ee48a23debc8c849610f3eb67d582c4a
ShanuChahal48 <shanuchahal48@gmail.com>
2025-09-28 21:20:12 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'https://soloyolo.onrender.com',
+        port: '',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
-
-module.exports = {
-  // ...existing config...
-  images: {
-    domains: ['localhost'],
-  },
-};
