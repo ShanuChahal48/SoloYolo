ae3326e59760dcfb0e7bc2f0cb5a43477f30cbb1
shanuchahal48 <shanuchahal@Shanus-MacBook-Air.local>
2025-09-29 19:35:53 +0530

    fix cloudinary issues

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,18 +1,14 @@
 export default ({ env }) => ({
-		upload: (() => {
-			const config = {
-				provider: 'cloudinary',
-				providerOptions: {
-					cloud_name: env('CLOUDINARY_NAME'),
-					api_key: env('CLOUDINARY_KEY'),
-					api_secret: env('CLOUDINARY_SECRET')
-				},
-				actionOptions: {
-					upload: {},
-					delete: {},
-				},
-			};
-			console.log('[Strapi] Cloudinary upload provider config loaded:', config.providerOptions);
-			return config;
-		})(),
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET'),
+    },
+    actionOptions: {
+      upload: {},
+      delete: {},
+    },
+  },
 });
